d142239ce3929b55334d002b679415ade269818e
Adriano Bacha <abacha@gmail.com>
2012-01-11 00:23:36 -0300

    episodes parser

--- parser.rb
+++ parser.rb
@@ -9,12 +9,18 @@ class Crawler
     @servers = {
       "good_wife" => [ "en.wikipedia.org",  "/wiki/List_of_The_Good_Wife_episodes" ] 
     }
+    @regex_episodes = Regexp.new(/<td class="summary" [^>]+>.*?<b>(.*?)<\/b>.*?<span[^>]+>([0-9-]+)<\/span>/m)
   end
 
   def get(serie)
     File.exists?(serie + ".cache") ? cache(serie) : web(serie)
   end
 
+  def parse_episodes(serie)
+    get(serie).scan(@regex_episodes)
+  end
+
+  private
   def web(serie)
     raw = Net::HTTP.get(@servers[serie][0], @servers[serie][1])
     File.open(serie + ".cache", 'w') { |f| f.write(raw) }
@@ -26,10 +32,11 @@ class Crawler
   end
 
 end
-c = Crawler.new
-html_raw = c.get("good_wife")
 
-episodes = html_raw.scan(/<td class="summary" [^>]+>.*?<b>(.*?)<\/b>.*?<span[^>]+>([0-9-]+)<\/span>/m)
+
+
+c = Crawler.new
+episodes = c.parse_episodes("good_wife")
 next_episode = []
 episodes.reverse_each { |episode|
   date_test = Date.parse(episode[1])
