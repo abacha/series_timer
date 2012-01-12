a737768318eed3abd07151b70d56c1cc7847bb7b
Adriano Bacha <abacha@gmail.com>
2012-01-12 12:14:50 -0300

    refactoring with review tips

--- lib/crawler.rb
+++ lib/crawler.rb
@@ -3,21 +3,23 @@ require 'net/http'
 
 class Crawler
 
-  def initialize
-    @regex_episodes = 
-      Regexp.new(/<td>(\d+)<\/td>.*?<td class="summary" [^>]+>(?:"<b>)?(?:<a [^>]+>)?([\w\s]*)(?:<\/a>)?(?:<\/b>")?.*?<span[^>]+>([0-9-]+)<\/span>/m)
+  REGEX_EPISODES = 
+    Regexp.new(/<td>(\d+)<\/td>.*?<td class="summary" [^>]+>(?:"<b>)?(?:<a [^>]+>)?([\w\s]*)(?:<\/a>)?(?:<\/b>")?.*?<span[^>]+>([0-9-]+)<\/span>/m)
+
+  def parse_episodes(serie)
+    renamed_serie = serie.gsub(" ", "_")
+    episodes = []
+    get(renamed_serie).scan(REGEX_EPISODES) do |e| 
+      episodes << Episode.new(serie, *e) 
+    end
+    episodes
   end
 
+  private
   def get(serie)
     File.exists?(get_cache_file(serie)) ? cache(serie) : web(serie)
   end
 
-  def parse_episodes(serie)
-    serie.gsub!(" ", "_")
-    get(serie).scan(@regex_episodes)
-  end
-
-  private
   def get_cache_file(serie)
     Dir.mkdir("/tmp/cache") if !File.directory?("/tmp/cache/")
     "/tmp/cache/" + serie + ".cache"
--- lib/series_manager.rb
+++ lib/series_manager.rb
@@ -1,24 +1,42 @@
 require_relative 'crawler'
 require 'date'
+require 'pry'
 
 class SeriesManager
 
   def get_next_episode(serie)
     crawler       = Crawler.new
     episodes      = crawler.parse_episodes(serie)
-    next_episode  = []
+    next_episode  = Episode.new
 
     episodes.reverse_each do |episode|
-      break if Date.parse(episode[2]) < Date.today && !next_episode.empty?
+      break if episode.date < Date.today && !next_episode.empty?
       next_episode = episode
     end
-    Date.parse(next_episode[2]) > Date.today ? next_episode : []
+    next_episode.date > Date.today ? next_episode : nil
   end
 
   def get_countdown_next_episode(serie)
     episode = get_next_episode(serie)
     return if episode.empty?
-    "S00" + ("e%02d" % episode[0].to_i) + " - " + episode[1] + parse_date(DateTime.parse(episode[2]))
+    "S00" + ("e%02d" % episode.number) + " - " + episode.title + episode.date
   end
 
 end
+
+class Episode < Struct.new(:serie, :number_raw, :title, :date_raw)
+  
+  def date
+    @date ||= Date.parse date_raw
+  end
+
+  def number
+    @number ||= number_raw.to_i
+  end
+
+  def empty?
+    number_raw == nil
+  end
+
+end
+
