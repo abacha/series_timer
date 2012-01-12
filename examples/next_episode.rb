47e6fb34edb1e6021191d2e9e3115581751d1ecd
Adriano Bacha <abacha@gmail.com>
2012-01-12 16:20:36 -0300

    adding one more test case (TBBT); seriesmanager now is a module

--- examples/next_episode.rb
+++ examples/next_episode.rb
@@ -4,9 +4,9 @@ require 'rubygems'
 require 'net/http'
 require_relative '../lib/series_manager'
 
-manager = SeriesManager.new
-p manager.get_next_episode("The Good Wife")
-p manager.get_next_episode("Two and a Half Men")
-p manager.get_next_episode("House")
-p manager.get_next_episode("Dexter")
+p SeriesManager.get_next_episode("The Good Wife")
+p SeriesManager.get_next_episode("Two and a Half Men")
+p SeriesManager.get_next_episode("House")
+p SeriesManager.get_next_episode("Dexter")
+p SeriesManager.get_next_episode("The Big Bang Theory")
 binding.pry
