4325ac9b343cbb773bf9f515f888d291651a5b0a
Adriano Bacha <abacha@gmail.com>
2012-01-12 16:23:28 -0300

    changing classes to modules

--- examples/next_episode.rb
+++ examples/next_episode.rb
@@ -8,4 +8,3 @@ p SeriesManager.get_next_episode("Two and a Half Men")
 p SeriesManager.get_next_episode("House")
 p SeriesManager.get_next_episode("Dexter")
 p SeriesManager.get_next_episode("The Big Bang Theory")
-binding.pry
