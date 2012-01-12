ed089d383659a17973d8ec40427d9eec410085e8
Adriano Bacha <abacha@gmail.com>
2012-01-12 12:14:58 -0300

    refactoring with review tips

---
+++ examples/next_episode.rb
@@ -0,0 +1,7 @@
+require_relative '../lib/series_manager'
+
+manager = SeriesManager.new
+p manager.get_next_episode("The Good Wife")
+p manager.get_next_episode("Two and a Half Men")
+p manager.get_next_episode("House")
+p manager.get_next_episode("Dexter")
