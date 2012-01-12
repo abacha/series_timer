d1865ce3bd3955b17db688e27719e90edafbd841
Adriano Bacha <abacha@gmail.com>
2012-01-12 16:21:07 -0300

    changing classes to modules

--- lib/crawler.rb
+++ lib/crawler.rb
@@ -1,36 +1,40 @@
-class Crawler
+require_relative 'episode'
 
-  REGEX_EPISODES = 
-    Regexp.new(/<td>(\d+)<\/td>.*?<td class="summary" [^>]+>(?:"<b>)?(?:<a [^>]+>)?([\w\s]*)(?:<\/a>)?(?:<\/b>")?.*?<span[^>]+>([0-9-]+)<\/span>/m)
+module Crawler
 
-  def parse_episodes(serie)
-    renamed_serie = serie.gsub(" ", "_")
-    episodes = []
-    get(renamed_serie).scan(REGEX_EPISODES) do |e| 
-      episodes << Episode.new(serie, *e) 
+  class << self
+    REGEX_EPISODES = 
+      Regexp.new(/<td>(\d+)<\/td>.*?<td class="summary" [^>]+>(?:"<b>)?(?:<a [^>]+>)?([\w\s]*)(?:<\/a>)?(?:<\/b>")?.*?<span[^>]+>([0-9-]+)<\/span>/m)
+
+    def parse_episodes(serie)
+      renamed_serie = serie.gsub(" ", "_")
+      episodes = []
+      get(renamed_serie).scan(REGEX_EPISODES) do |episode| 
+        episodes << Episode.new(serie, *episode) 
+      end
+      episodes
     end
-    episodes
-  end
 
-  private
-  def get(serie)
-    File.exists?(get_cache_file(serie)) ? cache(serie) : web(serie)
-  end
+    private
+    def get(serie)
+      File.exists?(get_cache_file(serie)) ? cache(serie) : web(serie)
+    end
 
-  def get_cache_file(serie)
-    Dir.mkdir("/tmp/cache") if !File.directory?("/tmp/cache/")
-    "/tmp/cache/" + serie + ".cache"
-  end
+    def get_cache_file(serie)
+      File.join(File.dirname(__FILE__), "../cache/") + serie + ".cache"
+    end
 
-  def web(serie)
-    raw = Net::HTTP.get("en.wikipedia.org", 
-                        "/wiki/List_of_" + serie + "_episodes")
-    File.open(get_cache_file(serie), 'w') { |f| f.write(raw) }
-    raw
-  end
+    def web(serie)
+      raw = Net::HTTP.get("en.wikipedia.org", 
+                          "/wiki/List_of_" + serie + "_episodes")
+      File.open(get_cache_file(serie), 'w') { |f| f.write(raw) }
+      raw
+    end
 
-  def cache(serie)
-    File.open(get_cache_file(serie), "r").read
+    def cache(serie)
+      File.open(get_cache_file(serie), "r").read
+    end
+
   end
 
 end
--- lib/series_manager.rb
+++ lib/series_manager.rb
@@ -1,40 +1,26 @@
 require_relative 'crawler'
 
-class SeriesManager
+module SeriesManager
 
-  def get_next_episode(serie)
-    crawler       = Crawler.new
-    episodes      = crawler.parse_episodes(serie)
-    next_episode  = Episode.new
+  class << self
 
-    episodes.reverse_each do |episode|
-      break if episode.date < Date.today && !next_episode.empty?
-      next_episode = episode
+    def get_next_episode(serie)
+      episodes      = Crawler.parse_episodes(serie)
+      next_episode  = Episode.new
+
+      episodes.reverse_each do |episode|
+        break if episode.date < Date.today && !next_episode.empty?
+        next_episode = episode
+      end
+      next_episode.date > Date.today ? next_episode : nil
     end
-    next_episode.date > Date.today ? next_episode : nil
-  end
 
-  def get_countdown_next_episode(serie)
-    episode = get_next_episode(serie)
-    return if episode.empty?
-    "S00" + ("e%02d" % episode.number) + " - " + episode.title + episode.date
-  end
+    def get_countdown_next_episode(serie)
+      episode = get_next_episode(serie)
+      return if episode.empty?
+      "S00" + ("e%02d" % episode.number) + " - " + episode.title + episode.date
+    end
 
-end
-
-class Episode < Struct.new(:serie, :number_raw, :title, :date_raw)
-  
-  def date
-    @date ||= Date.parse date_raw
-  end
-
-  def number
-    @number ||= number_raw.to_i
-  end
-
-  def empty?
-    number_raw == nil
   end
 
 end
-
