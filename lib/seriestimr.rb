28ff7ee33d3606a93d1b6bc8def342d8d1bc5f91
Adriano Bacha <abacha@gmail.com>
2012-01-27 09:25:16 -0300

    lib file

---
+++ lib/seriestimr.rb
@@ -0,0 +1,32 @@
+require_relative '../lib/seriestimr/series_manager'
+
+module SeriesTimr
+  class Application
+    
+    def initialize(argv)
+      @params, @series = parse_options(argv)
+    end
+
+    def parse_options(argv)
+      params = {}
+      parser = OptionParser.new
+      parser.on("-c") { params[:episodes] = :countdown }
+      parser.on("-l") { params[:episodes] = :last }
+      series = parser.parse(argv)
+      [params, series]
+    end
+
+    def run
+      @series.each do |serie|
+        case @params[:episodes]
+          when :last
+            puts SeriesManager.last_episode(serie)
+          when :countdown
+            puts SeriesManager.countdown(serie)
+          end
+      end
+    end
+
+  end
+end
+
